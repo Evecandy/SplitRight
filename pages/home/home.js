370181f4e1bb177bede6060b30e619d592085219
Eve Candy Mutunga <evecandymutunga@gmail.com>
2025-05-01 17:15:01 +0300

    Make the home page a dynamic page

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,73 +2,47 @@
 
 Page({
   data: {
-    groups: [
-      {
-        id: 1,
-        name: "Weekend Trip",
-        amount: "12,500",
-        members: 5,
-        expiry: "Apr 15, 2025"
-      },
-      {
-        id: 2,
-        name: "Office Lunch Pool",
-        amount: "3,200",
-        members: 8,
-        expiry: "Dec 31, 2025"
-      }
-    ]
+    // 
+    groups:""
   },
   onLoad(){
     my.showLoading();
     //making API call
     my.request({
-      url: 'https://mocki.io/v1/b9745da7-416d-48f9-9d50-7fc50e6b50ec',
+      url: 'https://mocki.io/v1/ea0e4b05-5d78-4baa-adf6-41f62e38862e',
       method: 'GET',
       data: {
         //payload for a POST request
       },
       dataType: 'json',
-      success: function(res) {
-        // my.alert({content: 'success'});
-        my.hideLoading()
-        console.log("api response",JSON.stringify(res.data, null,2))
-
-        //Updating groups data
-        this.setData({
-          groups: res.data
-        })
-        console.log("updated groups data",this.data.groups)
-        // my.alert({
-        //   title: 'success',
-        //   content: res.data,
-        //   buttonText: 'ok',
-        //   success: () => {
-            
-        //   },
-        //   fail: () => {
-            
-        //   },
-        //   complete: () => {
-            
-        //   }
-        // });
-      },
-      fail: function(res) {
-        my.alert({content: 'fail'});
-      },
+      success: (res) => {  // Arrow function preserves 'this'
+      // my.alert({content: 'success'});
+      my.hideLoading();
+      console.log("api response", JSON.stringify(res.data, null, 2));
+      // const groupsData=JSON.stringify(res.data)
+      // Updating groups data works now
+      this.setData({
+        groups: res.data.groups
+      });
+      console.log("updated groups data", this.data.groups);
+    },
+    fail: (err) => {
+      my.hideLoading();
+      console.error("API request failed:", err);
+    },
       // complete: function(res) {
       //   my.hideLoading();
       //   my.alert({content: 'complete'});
       // }
     });
     
-    const task = my.request({url: 'https://httpbin.org/post'})
-    task.abort()
+    //  
   },
-  onViewTap(){
+  onViewTap(eve){
+    console.log("eve",eve.target.dataset.id)
+    
     my.navigateTo({
-      url: '/pages/groupDetail/groupDetails'
+      url: '/pages/groupDetails/groupDetails'
     })
   },
   onCreateTap(){
